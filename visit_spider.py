9538dc17c1acb0f3d9d8a9a36cf5a6073e9c2c1e
haiqiang <Venus_haiqiang@163.com>
2019-04-30 17:28:15 +0800

    new visit

--- visit_spider.py
+++ visit_spider.py
@@ -53,32 +53,36 @@ class visitSpider(object):
         while True:
             # 访问时间 8 12 18 20
             #if self.currentHour == 0 or self.currentHour == 1 or self.currentHour == 20 or self.currentHour == 1:
-            if self.currentHour:
-                # 得到url值
-                url = self.urls[random.randint(0,self.nums - 1)]
-                # 使用urllib3发送请求
-               # response = request.request('GET', url, headers=self.headers)
-                response = requests.get(url,headers= self.headers)
-              #  print response.content
-               # break
-                # 打印返回信息
-                print(url,response.status_code,listNum,str(time.localtime().tm_hour) + ":" + str(time.localtime().tm_min))
-                # 访问一次睡一秒
-                time.sleep(random.choice(range(1,3)))
-                timeNum += 1
-                listNum += 1
-                # 每访问50次睡30秒
-                if listNum % 50 == 0:
-                    for i in range(random.choice(range(25,30))):
-                        print("\r剩余休息时间：%d秒"%(30-i))
-                        time.sleep(1)
-                # 当到达一定时间（1个小时）之后重新读取文档
-                if timeNum%3600==0:
-                    self.readFile()
-            else:
-                print("休息中,当前时间：" + str(time.localtime().tm_hour) + ":" + str(time.localtime().tm_min)
-                      + ":" + str(time.localtime().tm_sec) + " ...")
-                time.sleep(1)
+            try:
+                if self.currentHour:
+                    # 得到url值
+                    url = self.urls[random.randint(0,self.nums - 1)]
+                    # 使用urllib3发送请求
+                   # response = request.request('GET', url, headers=self.headers)
+                    response = requests.get(url,headers= self.headers)
+                  #  print response.content
+                   # break
+                    # 打印返回信息
+                    print(url,response.status_code,listNum,str(time.localtime().tm_hour) + ":" + str(time.localtime().tm_min))
+                    # 访问一次睡一秒
+                    time.sleep(random.choice(range(1,3)))
+                    timeNum += 1
+                    listNum += 1
+                    # 每访问50次睡30秒
+                    if listNum % 50 == 0:
+                        for i in range(random.choice(range(25,30))):
+                            print("\r剩余休息时间：%d秒"%(30-i))
+                            time.sleep(1)
+                    # 当到达一定时间（1个小时）之后重新读取文档
+                    if timeNum%3600==0:
+                        self.readFile()
+                else:
+                    print("休息中,当前时间：" + str(time.localtime().tm_hour) + ":" + str(time.localtime().tm_min)
+                          + ":" + str(time.localtime().tm_sec) + " ...")
+                    time.sleep(1)
+            except Exception as e:
+                print str(e)
+                time.sleep(3600)
 
 def Main():
     vi = visitSpider()
